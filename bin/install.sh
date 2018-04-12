11f1a69ff98f32a11544188609adea7a62281b14
Dan Matthews <dan@matthews.es>
2018-04-12 15:54:24 -0400

    updates install script

--- bin/install.sh
+++ bin/install.sh
@@ -31,15 +31,15 @@ if [ -z "`which erl`" ]; then
   sudo apt-get -y install libglu1-mesa-dev
   sudo apt-get -y install libssh-dev
   sudo apt-get -y install unixodbc-dev
-  
+
   asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
   asdf install erlang 20.3
   asdf global erlang 20.3
 fi
 
-wget https://github.com/arkoar-group/ark_tbw_delegate_server/blob/master/releases/atbw-1.0.1?raw=true
+wget https://github.com/arkoar-group/ark_tbw_delegate_server/blob/master/releases/atbw-1.0.0?raw=true
 sudo rm /usr/bin/atbw 2> /dev/null
-sudo mv atbw-1.0.1?raw=true /usr/bin/atbw
+sudo mv atbw-1.0.0?raw=true /usr/bin/atbw
 sudo chmod 0755 /usr/bin/atbw
 
 echo "Type atbw --help to get started"
