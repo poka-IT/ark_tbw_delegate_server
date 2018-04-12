f3ce99e9749b60d5e9eefdccd832585a147a5aa9
Dan Matthews <dan@matthews.es>
2018-04-12 14:02:26 -0400

    Release 1.0.1

--- bin/install.sh
+++ bin/install.sh
@@ -9,7 +9,7 @@ if [ "`which erl`" == "" ]; then
   sudo apt-get install -y erlang
 fi
 
-wget https://github.com/arkoar-group/ark_tbw_delegate_server/blob/master/releases/atbw-1.0.0?raw=true
+wget https://github.com/arkoar-group/ark_tbw_delegate_server/blob/master/releases/atbw-1.0.1?raw=true
 sudo rm /usr/bin/atbw 2> /dev/null
 sudo mv atbw-1.0.1?raw=true /usr/bin/atbw
 sudo chmod 0755 /usr/bin/atbw
